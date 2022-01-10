5c06ae9fd7f16395165cd5e2d08d6ace4f8884fd
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-01-11 00:20:24 +0900

    session02 basic types

---
+++ basic_type/functions.ts
@@ -0,0 +1,47 @@
+//function and type
+function add(n1: number, n2: number): number{ //return type과 맞는 type을 지정해준다. 그렇지만 굳이 적어줄 필요는 없다.
+    //return n1.toString() + n2.toString(); <--이렇게 지정하면 add 함수도 타입을 string으로 인식
+    return n1 + n2;
+}
+
+function printResult(num: number): void{ // 이 함수의 type은 void == return statement x, 이 함수는 아무것도 return하지 않는다.  
+    console.log('Result: '+ num);
+    // void for demo purposes
+    //void가 기술적으로는 undefined라고 return 하는데,
+    //typescript에서는 undefined도 type <- 꼭 써야하는 상황이고 확신할 수 있을 때만 쓰기!
+    //함수 타입을 undefined로 정하면 return; 이 있어야한다. void 일때 return;을 써도 에러가 나지 않고 어지간해서는 void를 쓴다.
+}
+
+                                            // 콜백함수 만들고 타입 지정하기 
+function addAndHandler(n1: number, n2: number, cb: (num: number)=> void){
+   
+   
+    const result = n1 + n2;
+    cb(result);
+    //  따라서 이 함수에서는 result type 있지만, 콜백함수 cd에는 result type은 무시된다.
+    // 콜백 함수는 자신이 전달되는 인수가 반환 값을 기대하지 않는 경우에도 값을 반환할 수 있습니다.
+    
+}
+
+console.log(printResult(add(5, 12)));
+
+//let someValue: undefined; // 이것도 타입! 
+//return 하는 value가 없을 땐 void가 대부분 경우에 쓰이는 기준
+//let combineValues; // 이 변수의 타입은 any여서 number를 써도 complie된다. 변수가 함수를 취급할 것이라고 분명히 해주기
+//let combineValues: Function; // type 
+
+let combineValues: (a: number, b: number) => number; // 정해진 두개의 파리미터 타입이 number 반환값도 number인 함수로 타입지정
+
+combineValues = add;
+//combineValues = printResult; //함수를 두개 쓰니까 undefined가 뜬다 더 구체적으로 타입 정해주기
+//combineValues = 5; ERROR!
+
+console.log(combineValues(8,8));
+
+addAndHandler(10, 20, (result)=>{ //callback 함수
+
+    console.log(result);
+
+    return result; // void 함수가 callback되므로 result 결과는 없다
+
+});
---
+++ basic_type/unknown-never.ts
@@ -0,0 +1,37 @@
+let userInput: unknown; 
+// 아직 사용자가 어떤 값을 입력할지 모르기 때문에 unknown type
+// 에러없이 어떤 값이든 변수에 담을 수 있다. 
+// any랑 작동방식이 다르지만 위와같은 특징은 같다.
+// string 또는 number를 쓸거라면 union type을 쓰는 것(let userInput: string | number)이 낫지만, 
+// unknown type은 타입을 미리 확인, 할수없는 것을 미리 알려준다.
+
+let userName: string;
+
+userInput = 5;
+userInput = 'Jang';
+
+//userName = userInput; // ERROR!
+// type unknown은 type string에 할당할 수 없기 때문이다.
+// userInput :any; 로 변경하면 에러가 사라진다.
+// any는 가장 폭넓게 쓰이는 type이고 type 확인이 필요없다.
+
+
+if(typeof userInput === 'string'){ //여기서 먼저 userInput은 string이라고 정의했기 때문에 에러가 발생하지 않는다. 
+    userName = userInput;
+}
+
+//never
+// 이 함수는 반환값이 없기 때문에 void type이라고 볼 수 있지만, 실제로 아무것도 반환하는 것이 아니다.
+// never를 반환하며, 값은 생성하지 않는다.
+// 이 함수는 항상 스크립트에 부딪힐 것이고 try를 사용해도 마찬가지이기 때문에 아무것도 return하지 않는다.
+// 그렇지만 함수의 type은 void이면서 never <- 어떤 것도 반환하지 않을 의도 
+function generateError(message: string, code: number):never{ //오류를 생성하는 유틸리티 함수를 만든다.
+  
+    throw {message: message, errorCode: code};
+    //while(true){}
+}
+
+const result = generateError('An error occured!' , 500); 
+console.log(result);
+
+
